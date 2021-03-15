a75ee6119a88d624504c8c9fed83deda5abe8d2a
KamilBaronowski <kamil.baronowski@wp.pl>
2021-03-15 15:55:13 +0100

    Commit II - some changes

--- src/main/java/com/sda/vehicle/VehicleApp.java
+++ src/main/java/com/sda/vehicle/VehicleApp.java
@@ -6,28 +6,28 @@ public class VehicleApp {
 
         Vehicle vehicle = new Vehicle();
         vehicle.run(90);
-        System.out.println("\t");
+        System.out.println();
 
-//        Car car01 = new Car();
-//        car01.addPassengers();
-//        car01.run(120);
-//        System.out.println("\t");
-//
-//        Motocycle motocycle = new Motocycle();
-//        System.out.println("\t");
-//        System.out.println("Dostęp z zewnątrz " + motocycle.getWheels());
-//        System.out.println("\t");
+        Car car01 = new Car();
+        car01.addPassengers();
+        car01.run(120);
+        System.out.println();
+
+        Motocycle motocycle = new Motocycle();
+        System.out.println();
+        System.out.println("Dostęp z zewnątrz " + motocycle.getWheels());
+        System.out.println();
 //
         vehicle = new Car();
         vehicle.run(100);
-        System.out.println("\t");
+        System.out.println();
 
         vehicle = new Motocycle();
         vehicle.run(180);
-        System.out.println("\t");
+        System.out.println();
 
-        Motocycle motocycle = new Motocycle();
-        runThisVehicle(motocycle);
+        Motocycle motocycle01 = new Motocycle();
+        runThisVehicle(motocycle01);
 
     }
     public static void  runThisVehicle (Vehicle vehicle){
