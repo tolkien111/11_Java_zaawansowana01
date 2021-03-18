a1e1bfbc3437c3c2f7d5fc507677dc4002e18ef2
KamilBaronowski <kamil.baronowski@wp.pl>
2021-03-18 18:23:48 +0100

    Zadanie 5 second und last part

--- src/main/java/com/sda/opp/zadania/Line.java
+++ src/main/java/com/sda/opp/zadania/Line.java
@@ -9,14 +9,21 @@ public class Line {
         this.point01 = point01;
         this.point02 = point02;
     }
-    public Line(float a, float b, float c, float d){
-        this.point01= new Point2D(a, b);
-        this.point02= new Point2D(c, d);
+    public Line(float x1, float y1, float x2, float y2){
+        this.point01= new Point2D(x1, y1);
+        this.point02= new Point2D(x2, y2);
     }
 
     public float lenghtLine(){
         float lenghtLine = (float) Math.sqrt(Math.pow(point01.getX() - point02.getX(), 2) + Math.pow(point01.getY()- point02.getY(), 2));
         return lenghtLine;
+    }
+
+    public Point2D centerPointofLine(){
+        float x3 = (point01.getX() + point02.getX())/2;
+        float y3 = (point01.getY() + point02.getY())/2;
+        Point2D centerPoint = new Point2D(x3, y3);
+        return centerPoint;
     }
 
     public Point2D getPoint01() {
@@ -34,6 +41,7 @@ public class Line {
     public void setPoint02(Point2D point02) {
         this.point02 = point02;
     }
+
 }
 
 
--- src/main/java/com/sda/opp/zadania/Point3dApp.java
+++ src/main/java/com/sda/opp/zadania/Point3dApp.java
@@ -7,32 +7,43 @@ public class Point3dApp {
     public static void main(String[] args) {
 
 
-
         Point2D point2D = new Point2D();
         point2D.setX(123.234f);
         point2D.setY(234.0978f);
         point2D.getX();
         point2D.toString();
-        point2D.setXY(123,123.234f);
+        point2D.setXY(123, 123.234f);
         point2D.getYX();
 
-        Point3D point3D = new Point3D(0.2f,5,1.1f);
+        Point3D point3D = new Point3D(0.2f, 5, 1.1f);
 
         point3D.setX(123.234f);
         point3D.setY(234.0978f);
         point3D.getX();
         point3D.toString();
-        point3D.setXY(123,123.234f);
+        point3D.setXY(123, 123.234f);
         point3D.getYX();
 
         System.out.println(point3D);
 
-
-        System.out.println(point3D);
-        float [] a = point3D.getXYZ();
+        float[] a = point3D.getXYZ();
         point3D.getXYZ();
         System.out.println(Arrays.toString(a));
         System.out.println(Arrays.toString(point3D.getXYZ()));
+        System.out.println("----------------------------------");
+
+        Point2D pointXY1 = new Point2D();
+        pointXY1.setXY(1, 2);
+        Point2D pointXY2 = new Point2D();
+        pointXY2.setXY(3, 5);
+        Line line = new Line(pointXY1, pointXY2);
+        System.out.println(line.lenghtLine());
+        System.out.println(line.centerPointofLine());
+
+
+        Line line1 = new Line(1, 2, 3, 5);
+        System.out.println(line1.lenghtLine());
+        System.out.println(line1.centerPointofLine());
 
     }
 }
